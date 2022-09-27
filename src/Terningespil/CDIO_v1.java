8c87e535334dfd4480a24bdb4216f66fe1482ffb
Mvergmann <mathiasvergmann@gmail.com>
2022-09-27 16:53:41 +0200

    Ekstra slag ved to ens rettet så det virker

--- src/Terningespil/CDIO_v1.java
+++ src/Terningespil/CDIO_v1.java
@@ -43,21 +43,34 @@ public class CDIO_v1 {
                 System.out.println("Du har slået to, 1'ere og mister alle dine point");
                 point1 = 0;
             }
-            if (point1>=40){
+            if (point1 >= 40) {
                 break;
             }
+
             while (kastsp1.getDie1Value() == kastsp1.getDie2Value()) { //Loop der giver ekstra slag hvis du slår to ens
-                System.out.println(spiller1 + " slog 2 ens! Kast igen! ");
+                System.out.println(spiller1 + " slog 2 ens! Tryk enter for at kaste igen! ");
+                Input.enter();
+
                 kastsp1.tur();
                 sum1 = kastsp1.sum;
                 System.out.println("du har slået " + kastsp1.getDie1Value() + " og " + kastsp1.getDie2Value());
                 point1 += sum1;
                 System.out.println("du har nu: " + point1 + " point.");
+
+                if (kastsp1.getDie1Value() == 1 && kastsp1.getDie2Value() == 1) {
+                    System.out.println("Du har slået to, 1'ere og mister alle dine point");
+                    point1 = 0;
+                }
+                if (point1 >= 40) {
+                    break;
+
+
+                }
             }
 
             System.out.println(spiller2 + " Tryk enter for at kaste tegningerne");
             Input.enter();
-            // Input statemnt for at spillet ikke forsætter uden player input.
+            // Input statement for at spillet ikke forsætter uden player input.
 
             System.out.println(spiller2 + " kaster terningerne");
             kastsp2.tur();
@@ -66,7 +79,7 @@ public class CDIO_v1 {
             point2 += sum2;
             System.out.println("du har nu: " + point2 + " point");
             System.out.println("");
-            //Spiller2's tur forløber på sammen måde some spiller1 og så starter loopet forfra med spiller1's tur hvis ingen spiller har 40 eller flere point.
+            //Spiller2's tur forløber på samme måde some spiller1 og så starter loopet forfra med spiller1's tur hvis ingen spiller har 40 eller flere point.
 
             if (kastsp2.getDie1Value() == 1 && kastsp2.getDie2Value() == 1) { //Sætter point til 0 hvis du slår 2 ettere.
                 System.out.println("Du har slået to, 1'ere og mister alle dine point");
@@ -74,28 +87,38 @@ public class CDIO_v1 {
             }
 
             while (kastsp2.getDie1Value() == kastsp2.getDie2Value()) { //Loop der giver ekstra slag hvis du slår to ens
-                System.out.println(spiller2 + " slog 2 ens! Kast igen! ");
+                System.out.println(spiller2 + " Tryk enter for at kaste tegningerne");
+                Input.enter();
+
                 kastsp2.tur();
                 sum1 = kastsp2.sum;
                 System.out.println("du har slået " + kastsp2.getDie1Value() + " og " + kastsp2.getDie2Value());
                 point1 += sum1;
                 System.out.println("du har nu: " + point2 + " point.");
+
+                if (kastsp2.getDie1Value() == 1 && kastsp2.getDie2Value() == 1) {
+                    System.out.println("Du har slået to, 1'ere og mister alle dine point");
+                    point1 = 0;
+                }
+                if (point1 >= 40) {
+                    break;
+                }
+
             }
+        }
+                if (point1 >= 40) {
+                    System.out.println(spiller1 + " har vundet!");
+                    System.out.println("Scoren blev: " + point1 + " point til " + spiller1 + " og " + point2 + " point til " + spiller2);
+                    // Dette if statemnent checker om det er spiller 1 der har 40 eller over
+                    //Derefter printer den en string der siger at spilleren har vundet og printer points for begge spiller
+                } else {
+                    System.out.println(spiller2 + " har vundet!");
+                    System.out.println("Scoren blev: " + point2 + " point til " + spiller2 + " og " + point1 + " point til " + spiller1);
+                    // Hvis spiller et ikke har vundet og vi er ude af while loopet må spiller 2 per definition være vinderen.
+                    //Printer sejrs besked og final pointtally.
+                }
+
 
         }
-        if (point1 >= 40) {
-            System.out.println(spiller1 + " har vundet!");
-            System.out.println("Scoren blev: " + point1 + " point til " + spiller1 + " og " + point2 + " point til " + spiller2);
-            // Dette if statemnent checker om det er spiller 1 der har 40 eller over
-            //Derefter printer den en string der siger at spilleren har vundet og printer points for begge spiller
-        }
-        else {
-            System.out.println(spiller2 + " har vundet!");
-            System.out.println("Scoren blev: " + point2 + " point til " + spiller2 + " og " + point1 + " point til " + spiller1);
-            // Hvis spiller et ikke har vundet og vi er ude af while loopet må spiller 2 per definition være vinderen.
-            //Printer sejrs besked og final pointtally.
-        }
+
     }
-
-}
-
