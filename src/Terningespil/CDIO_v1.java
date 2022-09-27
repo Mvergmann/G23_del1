80674c23a6bdeb1315d22df1defead72a59ccd09
nikl2513 <nikl2513@outlook.dk>
2022-09-27 11:26:42 +0200

    Tilføjet kommentare og implementeret enter metoden til CDIO1 version1

--- src/Terningespil/CDIO_v1.java
+++ src/Terningespil/CDIO_v1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CDIO_v1 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // Kalder Scanner class til brug længer nede i metoden.
 
         int sum1;
         int sum2;
@@ -13,15 +13,21 @@ public class CDIO_v1 {
         int point2 = 0;
         Tur kastsp1 = new Tur();
         Tur kastsp2 = new Tur();
+        // Her sættes spiller points til 0 og tur metoden kaldes fra Tur classen denne metode kaster to tegninger og returnere summen.
 
 
         System.out.println("Skriv navnet på spiller 1: ");
         String spiller1 = sc.nextLine();
         System.out.println("Skriv navnet på spiller 2: ");
         String spiller2 = sc.nextLine();
-
+        // Her spørger programmet om input fra spilleren i form a en string som er navn på spiller 1 og 2
 
         while (point1 < 40 && point2 < 40) {
+            // Dette while loop bliver ved med at loope de to spillers tur indtil den ende spiller går over 40 points
+
+            System.out.println(spiller1 + " Tryk enter for at kaste tegningerne");
+            Input.enter();
+            // Dette har til formål at skabe breaks i spillet for at spillet ikke bar kører fra start til slut efter spillerne indtaster navn.
 
             System.out.println(spiller1 + " kaster terningerne");
             kastsp1.tur();
@@ -29,7 +35,13 @@ public class CDIO_v1 {
             System.out.println("du har slået " + kastsp1.getDie1Value() + " og " + kastsp1.getDie2Value());
             point1 += sum1;
             System.out.println("du har nu: " + point1 + " point.");
+            System.out.println("");
+            //Her sker selve spiller turen hvor der kastes to tegninger med tur metoden og de ligges sammen med spillerens nuværne points
+            // Det tome print statement bruges til at lave en blank linje mellem spiller turen og det næste output.
 
+            System.out.println(spiller2 + " Tryk enter for at kaste tegningerne");
+            Input.enter();
+            // Input statemnt for at spillet ikke forsætter uden player input.
 
             System.out.println(spiller2 + " kaster terningerne");
             kastsp2.tur();
@@ -37,16 +49,22 @@ public class CDIO_v1 {
             System.out.println("du har slået " + kastsp2.getDie1Value() + " og " + kastsp2.getDie2Value());
             point2 += sum2;
             System.out.println("du har nu: " + point2 + " point");
-
-            if (point1 > 40) {
-                System.out.println(spiller1 + " har vundet!");
-                System.out.println("Scoren blev: " + spiller1 + " point og " + spiller2 + " point");
-            } else {
-                System.out.println(spiller2 + " har vundet!");
-                System.out.println("Scoren blev: " + point1 + " point til " + spiller1 + " og " + point2 + " point til " + spiller2);
-            }
+            System.out.println("");
+            //Spiller2's tur forløber på sammen måde some spiller1 og så starter loopet forfra med spiller1's tur hvis ingen spiller har 40 eller flere point.
         }
+        if (point1 >= 40) {
+            System.out.println(spiller1 + " har vundet!");
+            System.out.println("Scoren blev: " + spiller1 + " point og " + spiller2 + " point");
+            // Dette if statemnent checker om det er spiller 1 der har 40 eller over
+            //Derefter printer den en string der siger at spilleren har vundet og printer points for begge spiller
 
+        } else {
+            System.out.println(spiller2 + " har vundet!");
+            System.out.println("Scoren blev: " + point1 + " point til " + spiller1 + " og " + point2 + " point til " + spiller2);
+            // Hvis spiller et ikke har vundet og vi er ude af while loopet må spiller 2 per definition være vinderen.
+            //Printer sejrs besked og final point tally.
+        }
     }
+
 }
 
