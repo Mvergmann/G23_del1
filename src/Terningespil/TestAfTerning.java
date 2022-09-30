91d04812be32c0deb628f13cb29cbdd9941c803f
Mvergmann <mathiasvergmann@gmail.com>
2022-09-30 10:17:58 +0200

    Grammatiske rettelse

--- src/Terningespil/TestAfTerning.java
+++ src/Terningespil/TestAfTerning.java
@@ -19,7 +19,7 @@ public class TestAfTerning {
         int sum11 = 0;
         int sum12 = 0;
         /*
-         Her definineres en række variable af type int som bruges til at tælle antal gange tegningerne slår
+         Her defineres en række variabler af typen int som bruges til at tælle antal gange tegningerne slår
          forskellige resultater
          */
         System.out.println("Vælg antal terninger for testen: ");
@@ -27,7 +27,7 @@ public class TestAfTerning {
 
 
         for (antGang = 0; antGang < antal; antGang++) {
-            //for loopet roller terningen ti tusind gange for at bedømme om slagende er "Random"
+            //for loopet slår terningen ti tusind gange for at bedømme om resultatet er statistisk tilfældigt
             die1.roll();
             die2.roll();
             System.out.println(die1.getFaceValue() + die2.getFaceValue());
@@ -41,7 +41,8 @@ public class TestAfTerning {
             }
             if(die1.getFaceValue() + die2.getFaceValue()==3){
                 sum3++;
-            }  if(die1.getFaceValue() + die2.getFaceValue()==4){
+            }
+            if(die1.getFaceValue() + die2.getFaceValue()==4){
                 sum4++;
             }
             if(die1.getFaceValue() + die2.getFaceValue()==5){
@@ -68,11 +69,11 @@ public class TestAfTerning {
             if(die1.getFaceValue() + die2.getFaceValue()==12){
                 sum12++;
             }
-            // Disse if statements bruges til at tælle antalt af indstanser af forskellige resultater af terning slag
+            //Disse if statements bruges til at tælle antallet af indstanser af forskellige resultater af terning slag
         }
         float a = sum2;
         float b = toens;
-        //Laver sum2 og toens til nye variabler af type float. (int kan ikke give decimal tal og ville derfor give 0 når vi regner procent %
+        //Laver sum2 og toens til nye variabler af typen float. (int kan ikke give decimal tal og ville derfor give 0 når vi regner procent %
         System.out.println("Der blevet slået 2 ens: " + toens + " gange");
         System.out.println("Der blevet slået 2: " + sum2 + " gange");
         System.out.println("Der blevet slået 3: " + sum3 + " gange");
@@ -85,12 +86,12 @@ public class TestAfTerning {
         System.out.println("Der blevet slået 10: " + sum10 + " gange");
         System.out.println("Der blevet slået 11: " + sum11 + " gange");
         System.out.println("Der blevet slået 12: " + sum12 + " gange");
-        //Printer antalt af sum med forskellige værdier
+        //Printer antallet af sum med forskellige værdier
 
 
         System.out.println("Der er: "+((a/antal)*100)+"% chance for at slå to 1'er ");
         System.out.println("Der er: "+((b/antal)*100)+"% chance for at slå to ens ");
-        // regner og printer procent chancen for at terningen slår henholdstvis to 1'er og to ens.
+        // regner og printer procent chancen for at terningen slår henholdsvis to 1'er og to ens.
 
         System.out.println("Terning er testet på " +antal+ " antal slag");
     }
