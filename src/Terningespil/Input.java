8bc1556f4b19146e0dd8ca0b67965b4becfd7786
nikl2513 <nikl2513@outlook.dk>
2022-09-30 10:17:27 +0200

    skriver kommentarer til input class

--- src/Terningespil/Input.java
+++ src/Terningespil/Input.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Input
 {
-    public static String enter() //Laver et scanner objekt og lader den vente på et input af typen nextLine fra keyboardet
+    public static String enter()                //Laver et scanner objekt og lader den vente på et input af typen nextLine fra keyboardet
     {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        return s;
+        Scanner sc = new Scanner(System.in);    //Kalder et nyt scanner object
+        String s = sc.nextLine();               //Vi laver en ny string der hedder s som bliver sat til at være det samme som den næste linje brugeren skriver
+        return s;                               //Den returnere string s
     }
-    public static int antalslag()
+    public static int antalslag()               //Laver en method der tæller de antal slag som brugeren vil slå
     {
-        Scanner antal = new Scanner(System.in);
-        int testantal = antal.nextInt();
-        return testantal;
+        Scanner antal = new Scanner(System.in); //Laver en nyt scanner object
+        int testantal = antal.nextInt();        //Laver en variabel der hedder testantal som er det samme som den næste int som brugeren skriver
+        return testantal;                       //Returnere testantal
     }
 
 }
